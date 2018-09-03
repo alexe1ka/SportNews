81ac5401d9af188f4f921b777e9d5e6f05eb1c8b
alexe1ka <leshka_09@bk.ru>
2018-09-03 19:18:27 +0300

    add model package

---
+++ app/src/main/java/com/alexe1ka/sportnews/model/events/Events.java
@@ -0,0 +1,22 @@
+package com.alexe1ka.sportnews.model.events;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Events {
+
+    @SerializedName("events")
+    @Expose
+    private List<Event> events = null;
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+}
