b8c9f32eb5fe92037e4993c748707c0506ef9e1a
alexe1ka <leshka_09@bk.ru>
2018-09-03 19:40:13 +0300

    add fragments

---
+++ app/src/main/java/com/alexe1ka/sportnews/NewsListFragment.java
@@ -0,0 +1,9 @@
+package com.alexe1ka.sportnews;
+
+import android.support.v4.app.Fragment;
+
+public class NewsListFragment extends Fragment {
+    public static NewsListFragment newInstance() {
+        return new NewsListFragment();
+    }
+}
