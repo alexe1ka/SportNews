6d33ea3ce50a9c44d3246301ed90a649c9b8fb80
alexe1ka <leshka_09@bk.ru>
2018-09-04 21:14:53 +0300

    add listener for get current elem in nav view

--- app/src/main/java/com/alexe1ka/sportnews/repository/NewsRepository.java
+++ app/src/main/java/com/alexe1ka/sportnews/repository/NewsRepository.java
@@ -36,7 +36,11 @@ public class NewsRepository {
         Events events = new Events();
         List<Event> eventList = new ArrayList<>();
         Event event = new Event("Title", "Coeff", "Time", "Place", "Preview", "Article");
+        Event event1 = new Event("Title1", "Coeff1", "Time1", "Place1", "Preview1", "Article1");
+        Event event2 = new Event("Title2", "Coeff2", "Time2", "Place2", "Preview2", "Article2");
         eventList.add(event);
+        eventList.add(event1);
+        eventList.add(event2);
         events.setEvents(eventList);
         eventsMutableLiveData.setValue(events);
         return eventsMutableLiveData;
--- app/src/main/java/com/alexe1ka/sportnews/view/fragments/NewsListFragment.java
+++ app/src/main/java/com/alexe1ka/sportnews/view/fragments/NewsListFragment.java
@@ -4,23 +4,31 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.alexe1ka.sportnews.R;
 import com.alexe1ka.sportnews.view.adapters.NewsRvAdapter;
 import com.alexe1ka.sportnews.viewmodel.NewsListViewModel;
 
-public class NewsListFragment extends Fragment {
+public class NewsListFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener {
+
     private NewsListViewModel mNewsListViewModel;
 
     private RecyclerView mNewsRv;
     private NewsRvAdapter mNewsRvAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private NavigationView mNavigationView;
 
 
     public static final String TAG = NewsListFragment.class.getSimpleName();
@@ -32,6 +40,8 @@ public class NewsListFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mNavigationView = getActivity().findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
 
 
         mNewsListViewModel = ViewModelProviders.of(this).get(NewsListViewModel.class);
@@ -52,5 +62,28 @@ public class NewsListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_news_list, container, false);
         return view;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_football) {
+            Toast.makeText(this.getContext(), "Football",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_hockey) {
+            Toast.makeText(this.getContext(), "Hockey",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_tennis) {
+            Toast.makeText(this.getContext(), "tennis",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_basketball) {
+            Toast.makeText(this.getContext(), "basketball",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_volleyball) {
+            Toast.makeText(this.getContext(), "volleyball",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_cybersport) {
+            Toast.makeText(this.getContext(), "cybersport",Toast.LENGTH_LONG).show();
+        }
+
+        DrawerLayout drawer =this.getActivity().findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
