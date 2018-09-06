63eea1d373741cd6f4180249f97e4d7266c6daf3
alexe1ka <leshka_09@bk.ru>
2018-09-06 03:33:55 +0300

    add toolbar for first fragment

--- app/src/main/java/com/alexe1ka/sportnews/view/fragments/EventDescriptionFragment.java
+++ app/src/main/java/com/alexe1ka/sportnews/view/fragments/EventDescriptionFragment.java
@@ -9,9 +9,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.alexe1ka.sportnews.R;
+import com.alexe1ka.sportnews.model.articles.Article;
 import com.alexe1ka.sportnews.model.articles.ArticleDescription;
 import com.alexe1ka.sportnews.viewmodel.NewsDescriptionViewModel;
 
@@ -20,14 +22,15 @@ public class EventDescriptionFragment extends Fragment {
     public static String URL_EXTRA = "url";
 
     private NewsDescriptionViewModel mNewsDescriptionViewModel;
+    private ListView mListView;
 
-    private TextView mTeam1Tv;
-    private TextView mTeam2Tv;
-    private TextView mTimeTv;
-    private TextView mTournamentTv;
-    private TextView mPredictionTv;
-    private TextView mHeadOfEventTv;
-    private TextView mTextOfEventTv;
+//    private TextView mTeam1Tv;
+//    private TextView mTeam2Tv;
+//    private TextView mTimeTv;
+//    private TextView mTournamentTv;
+//    private TextView mPredictionTv;
+//    private TextView mHeadOfEventTv;
+//    private TextView mTextOfEventTv;
 
     public static EventDescriptionFragment newInstance() {
         return new EventDescriptionFragment();
@@ -38,31 +41,36 @@ public class EventDescriptionFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         //в ответе с апи на самом деле не названия команда,а слова СТАТИСТИКА и ГИД
-        mTeam1Tv = getActivity().findViewById(R.id.art_team1_tv);
-        mTeam2Tv = getActivity().findViewById(R.id.art_team2_tv);
+//        mTeam1Tv = getActivity().findViewById(R.id.art_team1_tv);
+//        mTeam2Tv = getActivity().findViewById(R.id.art_team2_tv);
+//
+//        mTimeTv = getActivity().findViewById(R.id.art_time_tv);
+//
+//        mTournamentTv = getActivity().findViewById(R.id.art_tournament_tv);
+//
+//        mPredictionTv = getActivity().findViewById(R.id.art_prediction_tv);
+//
+//        mHeadOfEventTv = getActivity().findViewById(R.id.art_head_of_event_tv);
+//
+//        mTextOfEventTv = getActivity().findViewById(R.id.art_text_of_event_tv);
 
-        mTimeTv = getActivity().findViewById(R.id.art_time_tv);
+        mListView = getActivity().findViewById(R.id.articles_list_view);
 
-        mTournamentTv = getActivity().findViewById(R.id.art_tournament_tv);
-
-        mPredictionTv = getActivity().findViewById(R.id.art_prediction_tv);
-
-        mHeadOfEventTv = getActivity().findViewById(R.id.art_head_of_event_tv);
-
-        mTextOfEventTv = getActivity().findViewById(R.id.art_text_of_event_tv);
         String url = getActivity().getIntent().getStringExtra(URL_EXTRA);
         mNewsDescriptionViewModel = ViewModelProviders.of(this).get(NewsDescriptionViewModel.class);
         mNewsDescriptionViewModel.init(url);
         mNewsDescriptionViewModel.getArticleDescriptionMutableLiveData().observe(this, new Observer<ArticleDescription>() {
             @Override
             public void onChanged(@Nullable ArticleDescription articleDescription) {
-                mTeam1Tv.setText(articleDescription.getTeam1());
-                mTeam2Tv.setText(articleDescription.getTeam2());
-                mTimeTv.setText(articleDescription.getTime());
-                mTournamentTv.setText(articleDescription.getTournament());
-                mPredictionTv.setText(articleDescription.getPrediction());
-                mHeadOfEventTv.setText(articleDescription.getArticle().toString());
-                mTextOfEventTv.setText("");
+                ArrayAdapter<Article> itemsAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, articleDescription.getArticle());
+                mListView.setAdapter(itemsAdapter);
+//                mTeam1Tv.setText(articleDescription.getTeam1());
+//                mTeam2Tv.setText(articleDescription.getTeam2());
+//                mTimeTv.setText(articleDescription.getTime());
+//                mTournamentTv.setText(articleDescription.getTournament());
+//                mPredictionTv.setText(articleDescription.getPrediction());
+//                mHeadOfEventTv.setText(articleDescription.getArticle().toString());
+//                mTextOfEventTv.setText("");
             }
         });
 //        Toast.makeText(getContext(), url, Toast.LENGTH_LONG).show();
--- app/src/main/java/com/alexe1ka/sportnews/view/fragments/NewsListFragment.java
+++ app/src/main/java/com/alexe1ka/sportnews/view/fragments/NewsListFragment.java
@@ -9,8 +9,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -51,6 +53,8 @@ public class NewsListFragment extends Fragment implements NavigationView.OnNavig
         ((SportNewsApp) getActivity().getApplication()).setInternetConnectionListener(this);
         ((SportNewsApp) getActivity().getApplication()).setInternetErrorListener(this);
 
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
         mProgressBar = getActivity().findViewById(R.id.loading_pb);
         mProgressBar.setVisibility(View.GONE);
 
